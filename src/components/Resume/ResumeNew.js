35d1065a13f98881b08442712d2f49ee46beeab1
Balogun Razak <razak2free@gmail.com>
2023-03-17 00:12:34 +0100

    j

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -15,7 +15,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
-  // const [numPages, setNumPages] = useState(null);
+  const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
   useEffect(() => {
---
+++ src/components/dummy.js
@@ -0,0 +1,119 @@
+<Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        
+
+       
+       
+
+         
+
+  
+          
+         
+
+          {/* UpMobile */}
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={UPS}
+              isBlog={false}
+              title="UPS."
+              description="Ups is a Delivery Mobile Application that supports both ios and android its built with React Native"
+              link="https://github.com/razak1104/Ups"
+              linkBtn=""
+            />
+          </Col>
+
+          {/* GOfresh */}
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={GOfresh}
+              isBlog={false}
+              title="GoFresh."
+              description="GoFresh is a Food Mobile Application built with React Native"
+              link="https://github.com/razak1104/Fast_Food"
+              linkBtn=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Actech}
+              isBlog={false}
+              title="ACTEch Air-Condtioning Services"
+              description=' This app is built with Rect and react-icon'
+              linkBtn="https://google.com"
+            />
+          </Col>
+          {/* FoodWeb */}
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={foodApp}
+              isBlog={false}
+              title="Food-App"
+              description="A Food App Built Using React.JS, Styled Component for styling and a RestFul Api for Collection of Data"
+              link="https://github.com/razak1104/food-app"
+              linkBtn=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Chatify"
+              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              link="https://github.com/razak1104/Hulu"
+              linkBtn="https://google.com"
+            />
+          </Col>
+
+
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Hulu}
+              isBlog={false}
+              title="HULU 2.0"
+              description="Crated with Material Ui with little Bootstrap and fetch API from TMBD API "
+              link="https://github.com/razak1104/MERN_STACK"
+              linkBtn="https://google.com"
+            />
+          </Col>
+
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={jarastore}
+              isBlog={false}
+              title="Jara-Stote"
+              description="Crated with Material Ui with little Bootstrap and fetch API from TMBD API "
+              link="https://github.com/razak1104/MERN_STACK"
+              linkBtn="https://google.com"
+            />
+          </Col>
+
+
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ApiManagement}
+              isBlog={false}
+              title="Api-Management"
+              description='This is site for testing API'
+              linkBtn="https://google.com"
+            />
+          </Col>
+          
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={woozeeMovies}
+              isBlog={false}
+              title="Woozeee Movies"
+              description="Woozee Movies is built with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode ."
+              link="https://github.com/razak1104/Woozee-Movies"
+              linkBtn="https://google.com"
+            />
+          </Col>
+
+
+        </Row>
