70fa9f6f542229402ceb2cc823084e3e5880ba52
Balogun Razak <razak2free@gmail.com>
2023-05-14 20:43:52 +0100

    changes

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,44 +8,46 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hello, everyone! My name is{" "}
-            <span className="purpleAbt">Razak Balogun,</span> and I'm a
-            passionate
-            <span className="purpleAbt"> Software Engineer </span> hailing from{" "}
-            <span className="purpleAbt">Lagos, Nigeria.</span> I find great joy
-            in crafting innovative solutions through coding and constantly
-            pushing the boundaries of technology.
+            A talented <span className="purpleAbt"> Software Engineer </span>{" "}
+            hailing from the vibrant city of{" "}
+            <span className="purpleAbt">Lagos, Nigeria.</span>
+            With a passion for coding, Razak has honed his skills and expertise
+            in the field, making him a valuable asset in the world of
+            technology.
             <br />
-            In addition to my love for programming, I have a range of other
-            activities that bring excitement and balance to my life:
+            Beyond his coding prowess, Razak indulges in a variety of activities
+            that bring him joy and balance in life.
           </p>
           <div>
             <span className="about-activity">
-            <AiFillStar style={{ fontSize: "40px" }} />
+              <AiFillStar style={{ fontSize: "40px" }} />
               <p>
-                <span className="purpleAbt">Playing Games:</span> When I'm not immersed in lines of code, you'll
-                often find me exploring virtual worlds and engaging in
-                exhilarating gaming adventures. It's a fantastic way to unwind
-                and immerse myself in captivating narratives.
+                <span className="purpleAbt">Playing Games:</span> When I'm not
+                immersed in lines of code, you'll often find me exploring
+                virtual worlds and engaging in exhilarating gaming adventures.
+                It's a fantastic way to unwind and immerse myself in captivating
+                narratives.
               </p>
             </span>
             <span className="about-activity">
               <AiFillStar style={{ fontSize: "40px" }} />
               <p>
-              <span className="purpleAbt">Playing Basketball:</span>  As a sports enthusiast, I relish the
-                physical challenges and camaraderie that basketball provides.
-                Whether it's on the court or cheering on my favorite teams, the
-                sport fuels my competitive spirit and keeps me energized.
+                <span className="purpleAbt">Playing Basketball:</span> As a
+                sports enthusiast, I relish the physical challenges and
+                camaraderie that basketball provides. Whether it's on the court
+                or cheering on my favorite teams, the sport fuels my competitive
+                spirit and keeps me energized.
               </p>
             </span>
             <span className="about-activity">
-            <AiFillStar style={{ fontSize: "40px" }} />
+              <AiFillStar style={{ fontSize: "40px" }} />
               <p>
-              <span className="purpleAbt">Going To The Gym:</span> Maintaining a healthy lifestyle is crucial to
-                me, and the gym is my sanctuary for physical and mental
-                well-being. I believe in the power of exercise to not only
-                strengthen the body but also invigorate the mind, allowing me to
-                approach challenges with renewed vigor.
+                <span className="purpleAbt">Going To The Gym:</span> Maintaining
+                a healthy lifestyle is crucial to me, and the gym is my
+                sanctuary for physical and mental well-being. I believe in the
+                power of exercise to not only strengthen the body but also
+                invigorate the mind, allowing me to approach challenges with
+                renewed vigor.
               </p>
             </span>
           </div>
