a9ad7286f9b6a0193a9dc0eb7046a298bda47c6b
balogun1104 <razak2free@gmail.com>
2023-10-01 16:19:41 +0100

    changse;;

--- src/components/Projects/Webflow.js
+++ src/components/Projects/Webflow.js
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import AkaraHouse from "../../Assets/Projects/akaraHouse.png";
 import QuickChapters from "../../Assets/Projects/untitledui.png";
 import UntitledUi from "../../Assets/Projects/youtube.png";
+import ScreenStudio from "../../Assets/Projects/screenStudio.png";
 
 const Webflow = () => {
   return (
@@ -38,6 +39,17 @@ const Webflow = () => {
             title="Untitled Ui"
             description="This project involves the development of a cryptocurrency mobile application that provides real-time information about different cryptocurrencies to its users. The app will display information such as price, market cap, volume, and trends of the various cryptocurrencies. The app will also allow users to create a watchlist of their preferred cryptocurrencies."
             link="https://sky-template-layout-62708fb897f1e76a6ea.webflow.io/"
+            linkBtn=""
+          />
+        </Col>
+
+        <Col md={4} className="project-card">
+          <ProjectCard
+            imgPath={ScreenStudio}
+            isBlog={false}
+            title="Quick Chapters"
+            description="This project involves the development of a cryptocurrency mobile application that provides real-time information about different cryptocurrencies to its users. The app will display information such as price, market cap, volume, and trends of the various cryptocurrencies. The app will also allow users to create a watchlist of their preferred cryptocurrencies."
+            link="https://www.screen.studio/"
             linkBtn=""
           />
         </Col>
